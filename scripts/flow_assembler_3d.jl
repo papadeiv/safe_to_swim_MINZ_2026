35770346da3bd81d41d05c56551fba1147e6ec91
Davide Papapicco <davidepapapicco93@gmail.com>
2026-06-23 16:29:22 +1200

    finished assembler for flow_max

--- scripts/flow_assembler_3d.jl
+++ scripts/flow_assembler_3d.jl
@@ -30,7 +30,17 @@ data = ecoli_parser(filename, 2)
 
 # Match the dates between flow (daily) and E.coli (weekly) measurements
 #matching_idx = [findfirst(==(date), dates[(non_consecutive_idx[end]+1):end]) for date in data.data[:,1]]
-matching_idx = filter(!isnothing, [findfirst(==(date), dates) for date in data.data[:,1]])
-matched_dates = dates[matching_idx.-1]
-matched_flow = flow[matching_idx.-1]
-display(hcat(dates[matching_idx], matched_dates, matched_flow))
+matching_idx = filter(!isnothing, [findfirst(==(date), dates) for date in data.data[:,1]]) .- 1
+
+# Loop over the matching indices
+flow_matrix = Matrix{Any}(undef, length(matching_idx), 4)
+for (n, idx) in enumerate(matching_idx)
+        flow_matrix[n,1] = dates[idx]
+        flow_matrix[n,2] = flow[idx]
+        flow_matrix[n,3] = max(flow[idx],flow[idx-1],flow[idx-2])
+        flow_matrix[n,4] = flow[idx]+flow[idx-1]+flow[idx-2]
+end
+
+colnames = ["day", "1 day flow", "3 days max flow", "3 days total flow"]
+df = DataFrame(flow_matrix, Symbol.(colnames))
+CSV.write("../data/observations/flow_max/3.csv", df)
